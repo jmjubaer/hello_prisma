6158df7f5994dda6da35b672f26119701feb3708
jmjubaer <jmjubaer3927@gmail.com>
2026-01-04 10:24:33 +0600

    insert data into db via prisma

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
+import { prisma } from "./lib/prisma";
 
-const main = () => {
 
-}
-main()
+const main = async () => {
+    // const result = await prisma.user.create({
+    //     data: {
+    //         name: "jubaer",
+    //         email: "jubaer@gmail.com",
+    //         profilePhoto: "link",
+    //     },
+    // });
+    // console.log(result);
+    const findAll = await prisma.user.findMany();
+    console.log(findAll);
+};
+main();
