851fe0e40e8e6799e00c338072f320f2a6b480f1
jmjubaer <jmjubaer3927@gmail.com>
2026-01-04 12:59:28 +0600

    play update oparation in prisma

--- index.ts
+++ index.ts
@@ -14,28 +14,62 @@ const main = async () => {
     const findAll = await prisma.user.findMany();
     console.log(findAll);
 
-    // get all data with condition
-    const findAllWithCondition = await prisma.user.findMany({
-        where: {
-            id: 1,
-        },
-    });
-    console.log(findAllWithCondition);// return in a array
+    // // get all data with condition
+    // const findAllWithCondition = await prisma.user.findMany({
+    //     where: {
+    //         id: 1,
+    //     },
+    // });
+    // console.log(findAllWithCondition);// return in a array
 
-    // return single data in a object
-    const findOne = await prisma.user.findUnique({
-        where: {
-            id: 1,
-        },
-    });
-    console.log(findOne);
+    // // return single data in a object
+    // const findOne = await prisma.user.findUnique({
+    //     where: {
+    //         id: 1,
+    //     },
+    // });
+    // console.log(findOne);
 
-    // if get data return the data. if not get throw error
-    const findOneWithErrorHandle = await prisma.user.findUniqueOrThrow({
+    // // if get data return the data. if not get throw error
+    // const findOneWithErrorHandle = await prisma.user.findUniqueOrThrow({
+    //     where: {
+    //         id: 3,
+    //     },
+    // });
+    // console.log(findOneWithErrorHandle);
+
+    // update one data
+    // const updateUser = await prisma.user.update({
+    //     where: {
+    //         id: 1
+    //     },
+    //     data: {
+    //         name: "JM"
+    //     }
+    // })
+    // console.log(updateUser);
+
+    // update many data at a time and return update data count
+    const updateMany = await prisma.user.updateManyAndReturn({
         where: {
-            id: 3,
+            profilePhoto: "link",
+        },
+        data: {
+            profilePhoto: "https://image.com",
         },
     });
-    console.log(findOneWithErrorHandle);
+    console.log(updateMany);
+
+    // update many data at a time and return update data
+
+    const updateManyAndReturn = await prisma.user.updateManyAndReturn({
+        where: {
+            profilePhoto: "link",
+        },
+        data: {
+            profilePhoto: "https://image.com",
+        },
+    });
+    console.log(updateManyAndReturn);
 };
 main();
